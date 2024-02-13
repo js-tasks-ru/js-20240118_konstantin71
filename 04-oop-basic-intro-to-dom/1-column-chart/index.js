9c656de811b882a98bf8c954ec18fa5048e4ff4c
Иван Иванович <mail@email.ru>
2024-02-13 11:48:54 +0500

    Задание ColumnChart передено на наследование

--- 04-oop-basic-intro-to-dom/1-column-chart/index.js
+++ 04-oop-basic-intro-to-dom/1-column-chart/index.js
@@ -1,3 +1,5 @@
+import { createElementFromHTML, removeChildren } from "../../utils/html.js";
+
 export default class ColumnChart {
   constructor({
     data = [],
@@ -14,23 +16,32 @@ export default class ColumnChart {
     this._link = link;
 
     this.chartHeight = 50;
-    
-    this.render();
+
+    this.subElements = {};
+    this.element = null;
+
+    this.createElement();
     this._update();    
   }
 
-  render() {
-    this._element = UtilsHTML.createHTMLElement(this.template());
-    document.body.append(this._element);
+  createElement() {
+    this.element = createElementFromHTML(this.template());
+    this.selectSubElements();
+  }
+
+  selectSubElements() {
+    this.element.querySelectorAll('[data-element]').forEach(element => {
+      this.subElements[element.dataset.element] = element;
+    });
   }
 
   remove() {
-    this._element.remove();
+    this.element?.remove();
   }
 
   destroy() {
     this.remove();
-    this._element = null;
+    this.element = null;
   }
 
   getHeadingContent() {
@@ -39,7 +50,7 @@ export default class ColumnChart {
   
   template() {
     return `
-    <div class="column-chart ${this._data.length > 0 ? '' : 'column-chart_loading'}" style="--chart-height: 50">
+    <div class="column-chart ${this._data.length > 0 ? '' : 'column-chart_loading'}" style="--chart-height: ${this.chartHeight}">
       <div class="column-chart__title">
         ${this._label}
         <a href="${this._link}" class="column-chart__link">View all</a>
@@ -58,16 +69,16 @@ export default class ColumnChart {
   update(data) {
     this._data = data;
     this._update();
+
+    return data;
   }
 
   _update() {
-    const chart = this._element.querySelector('.column-chart__chart');
+    const chart = this.element.querySelector('.column-chart__chart');
 
-    UtilsHTML.removeChildren(chart);
+    removeChildren(chart);
 
-
-
-    for (const item of getColumnProps(this._data, this.chartHeight)) {
+    for (const item of this.getColumnProps(this._data, this.chartHeight)) {
       const tempElement = document.createElement('div');
         
       tempElement.style = `--value: ${item.value}`;
@@ -75,38 +86,24 @@ export default class ColumnChart {
 
       chart.append(tempElement);
     }
-  }
 
-
-  get element() {
-    return this._element;
-  }
-}
-
-function getColumnProps(data, chartHeight) {
-  const maxValue = Math.max(...data);
-  const scale = chartHeight / maxValue;
-  
-  return data.map(item => {
-    return {
-      percent: (item / maxValue * 100).toFixed(0) + '%',
-      value: String(Math.floor(item * scale))
-    };
-  });
-}
-
-class UtilsHTML {
-  static createHTMLElement(html) {
-    const tempElement = document.createElement('div');
-    tempElement.innerHTML = html;
-    return tempElement.firstElementChild; 
-  }
-
-  static removeChildren (element) {
-    while (element.firstElementChild) {
-      element.firstElementChild.remove();
+    if (this._data.length > 0) {
+      this.element.classList.remove('column-chart_loading');
+    } else {
+      this.element.classList.add('column-chart_loading');
     }
+  }
 
+  getColumnProps(data, chartHeight) {
+    const maxValue = Math.max(...data);
+    const scale = chartHeight / maxValue;
+    
+    return data.map(item => {
+      return {
+        percent: (item / maxValue * 100).toFixed(0) + '%',
+        value: String(Math.floor(item * scale))
+      };
+    });
   }
 }
 
--- 07-async-code-fetch-api-part-1/1-column-chart/index.js
+++ 07-async-code-fetch-api-part-1/1-column-chart/index.js
@@ -1,32 +1,23 @@
 import fetchJson from './utils/fetch-json.js';
 
+import BaseColumnChart from "../../04-oop-basic-intro-to-dom/1-column-chart/index.js";
 
 const BACKEND_URL = 'https://course-js.javascript.ru';
 
 // https://course-js.javascript.ru/api/dashboard/orders?from=2024-01-13T15:24:35.313Z&to=2024-02-12T15:24:35.313Z
 
-export default class ColumnChart {
+export default class ColumnChart extends BaseColumnChart {
   constructor({
     url = '',
-    label = '', 
+    label = '',
+    data = [],
     value = 200,
     link = '#',
     formatHeading = v => v
   } = {}) {
+    super({url, label, data, value, link, formatHeading});
 
-    this.data = null;
-    this.chartHeight = 50;
-
-    this._label = label;
-    this._value = value;
-    this._formatHeading = formatHeading;
-    this._link = link;
     this._url = url;
-
-    this.subElements = {};
-    this.element = null;
-
-    this.render();  
   }
 
   async update(from, to) {
@@ -36,103 +27,13 @@ export default class ColumnChart {
 
     const response = await fetch(url.href);
     const data = await response.json();
-        
-    this._update(Object.values(data));
+
+    this._data = Object.values(data);
+    this._update();
 
     return data;
   }
   
-  selectSubElements() {
-    this.element.querySelectorAll('[data-element]').forEach(element => {
-      this.subElements[element.dataset.element] = element;
-    });
-  }
-
-  render() {
-    this.element = UtilsHTML.createHTMLElement(this.template());
-    this.selectSubElements();
-  }
-  
-  remove() {
-      this.element?.remove();
-  }
-  
-  destroy() {
-    this.remove();
-    this.element = null;
-  }
-  
-  getHeadingContent() {
-    return this._formatHeading(this._value);
-  }
-    
-  template() {
-    return `
-      <div class="column-chart column-chart_loading" style="--chart-height: ${this.chartHeight}">
-        <div class="column-chart__title">
-        ${this._label}
-        <a href="${this._link}" class="column-chart__link">View all</a>
-        </div>
-        <div class="column-chart__container">
-          <div data-element="header" class="column-chart__header">${this.getHeadingContent()}</div>
-          <div data-element="body" class="column-chart__chart">
-            
-          </div>
-        </div>
-      </div>
-      `;
-  
-  }
-  
-  _update(data) {
-    const chart = this.element.querySelector('.column-chart__chart');
-  
-    UtilsHTML.removeChildren(chart);
-  
-    for (const item of getColumnProps(data, this.chartHeight)) {
-      const tempElement = document.createElement('div');
-          
-      tempElement.style = `--value: ${item.value}`;
-      tempElement.dataset.tooltip = item.percent;
-  
-      chart.append(tempElement);
-    }
-
-    if (data.length > 0) {
-      this.element.classList.remove('column-chart_loading');
-    } else {
-      this.element.classList.add('column-chart_loading');
-    }
-  }
-
 }
-  
-function getColumnProps(data, chartHeight) {
-  const maxValue = Math.max(...data);
-  const scale = chartHeight / maxValue;
-    
-  return data.map(item => {
-    return {
-      percent: (item / maxValue * 100).toFixed(0) + '%',
-      value: String(Math.floor(item * scale))
-    };
-  });
-}
-  
-class UtilsHTML {
-  static createHTMLElement(html) {
-    const tempElement = document.createElement('div');
-    tempElement.innerHTML = html;
-    return tempElement.firstElementChild; 
-  }
-  
-  static removeChildren (element) {
-    while (element.firstElementChild) {
-      element.firstElementChild.remove();
-    }
-  
-  }
-}
-  
   
   
